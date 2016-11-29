2f5c42edb9081a27fbc923d61d6c9f628339bee4
summerangel <summerlisi@163.com>
2016-11-29 18:29:11 +0800

    init vue project

---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+/**
+ * Created by summer on 16/11/29.
+ */
+import Vue from 'vue'
+import Vuex from 'vuex'
+import mutations from './mutations'
+import actions from './actions'
+
+Vue.use(Vuex);
+
+const state = {
+  totalTime: 0,
+  list: []
+};
+
+
+export default new Vuex.Store({
+  state,
+  mutations,
+  actions
+})
